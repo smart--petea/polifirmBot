a964ce41b17ed5fae1d5f4d548e33fcb8bb5708a
petru <smart__petea@mail.ru>
2016-10-12 17:04:45 +0300

    matchers

--- src/main/java/r/MyBean.java
+++ src/main/java/r/MyBean.java
@@ -8,9 +8,16 @@ import java.util.regex.Pattern;
 
 public class MyBean {
     private RestTemplate restTemplate = new RestTemplate();
+
     private Pattern categoryPattern = Pattern.compile("portfolio-wrap.*?href=\"(?<categoryUrl>[^\"]*)\"", Pattern.MULTILINE | Pattern.DOTALL);
+
     private Pattern trPattern = Pattern.compile("<tr>(?<trGroup>(.*?<td.*?(?<!<td)){3})</tr>", Pattern.MULTILINE | Pattern.DOTALL);
+
     private Pattern mailPattern = Pattern.compile("href=\"mailto:(?<mail>[^\"]*)\"");
+    private Pattern httpPattern = Pattern.compile("href=\"https?://(?<http>[^\"]*)\"");
+    private Pattern phonePattern = Pattern.compile("tel.:(?<phone>.*?)<br");
+    private Pattern namePattern = Pattern.compile("<h3>(?<name>.*?)</h3>");
+    private Pattern descriptionPattern = Pattern.compile("<h2>(?<description>.*?)</h2>", Pattern.MULTILINE | Pattern.DOTALL);
 
     public MyBean() {}
 
@@ -30,13 +37,32 @@ public class MyBean {
             String categoryBody = downloadPage("http://www.polish.ru/A01/ru.html");
             Matcher trMatcher = trPattern.matcher(categoryBody);
             trMatcher.find();
+
             while(trMatcher.find()) {
                 String trBody = trMatcher.group("trGroup");
     //            System.out.println(trBody);
 
-                Matcher mailMatcher = mailPattern.matcher(trBody);
-                if(mailMatcher.find()) {
-                    System.out.println(mailMatcher.group("mail"));
+                //Matcher mailMatcher = mailPattern.matcher(trBody);
+                //if(mailMatcher.find()) {
+                //    System.out.println(mailMatcher.group("mail"));
+                //}
+                //Matcher httpMatcher = httpPattern.matcher(trBody);
+                //if(httpMatcher.find()) {
+                //    System.out.println(httpMatcher.group("http"));
+                //}
+                //Matcher phoneMatcher = phonePattern.matcher(trBody);
+                //if(phoneMatcher.find()) {
+                //    System.out.println(phoneMatcher.group("phone"));
+                //}
+                //Matcher nameMatcher = namePattern.matcher(trBody);
+                //if(nameMatcher.find()) {
+                //    System.out.println(nameMatcher.group("name"));
+                //}
+                //
+                Matcher descriptionMatcher = descriptionPattern.matcher(trBody);
+                if(descriptionMatcher.find()) {
+                    System.out.println("----------------");
+                    System.out.println(descriptionMatcher.group("description"));
                 }
             }
         //}
